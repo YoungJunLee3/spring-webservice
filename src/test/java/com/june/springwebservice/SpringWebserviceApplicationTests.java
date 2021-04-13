d5b04e09d00191b6db3a10312982a02b81a58c14
DESKTOP-TVTAIRD\이영준 <lee8307@gmail.com>
2021-04-14 00:09:51 +0900

    2021-04-14 처음 커밋

---
+++ src/test/java/com/june/springwebservice/SpringWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.june.springwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
